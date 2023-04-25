d703ec58ca73bc297f1d71cc2a64281a79015ec0
53RG3 <FritschSe@free.fr>
2023-04-25 19:24:47 +0200

    Add files via upload
    Le mapping pour le cardealerluxe

---
+++ luxusdealer/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+
+game { 'gta5' }
+
+description 'Globbing Template made by PLOK'
+
+data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
+
+files { 'stream' }
+
+this_is_a_map "yes"
+
+
